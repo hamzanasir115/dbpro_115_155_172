1e43ae1b31561f009fc798d01e503e27eaf7d12f
Hamza <hamzanasir115>
2019-04-24 18:52:36 +0500

    Login complete with Register

--- MediaEncyclopediaInUrdu/MediaEncyclopediaInUrdu/Controllers/AccountController.cs
+++ MediaEncyclopediaInUrdu/MediaEncyclopediaInUrdu/Controllers/AccountController.cs
@@ -74,12 +74,14 @@ namespace MediaEncyclopediaInUrdu.Controllers
             string Password = null;
             foreach (var per in db.Accounts)
             {
-                if (per.UserName == model.Name && per.Password == model.Password)
+                if (per.Email == model.Name && per.Password == model.Password)
                 {
                     Name = per.UserName;
                     Password = per.Password;
+                    return RedirectToAction("UserProfile", "Manage");
+
                 }
-               
+
             }
             return View(model);
         }
@@ -162,25 +164,7 @@ namespace MediaEncyclopediaInUrdu.Controllers
             dbo.SaveChanges();
             return RedirectToAction("HomePage", "Home");
 
-            /*if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-            }*/
-
+           
             // If we got this far, something failed, redisplay form
             return View(model);
             
